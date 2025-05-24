c189b454066d2e5aca908dd7d1ebbf8e90596def
GHARBI Abdelhafidh <130412760+AbdelhafidhGHARBI@users.noreply.github.com>
2025-05-24 21:34:06 +0100

    feat: initialize Maven project structure and main entry point

--- src/main/java/tn/esprit/Main.java
+++ src/main/java/tn/esprit/Main.java
@@ -1,7 +1,11 @@
 package tn.esprit;
 
+import gestionReservation.ReservationService;
+import jakarta.xml.ws.Endpoint;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      Endpoint.publish("http://localhost:8888/ws/reservation", new ReservationService());
+//      System.out.println("Reservation Service is ready in http://http://localhost:8888/ws/reservation?WSDL");
     }
 }
